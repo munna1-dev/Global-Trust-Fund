cefacf8f472bc306cee8830bd0e3b389282e0d56
munna1-dev <liuh27148@gmail.com>
2026-08-20 01:21:46 +0000

    Initial project upload

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>Global Trust Fund</h1>
+      <p>Digital Banking Platform</p>
+
+      <div>
+        <button>Login</button>
+        <button>Create Account</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
